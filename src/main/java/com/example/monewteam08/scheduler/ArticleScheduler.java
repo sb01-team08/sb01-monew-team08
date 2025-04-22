7a446ab02bd2494e73a2d7648c3b337ffc5d1de2
eunseobb <seobseob0810@naver.com>
2025-04-22 15:15:59 +0900

    feat: 애플리케이션 실행 즉시 기사 가져오도록 초기화 기능 추가

--- src/main/java/com/example/monewteam08/scheduler/ArticleScheduler.java
+++ src/main/java/com/example/monewteam08/scheduler/ArticleScheduler.java
@@ -1,15 +1,23 @@
 package com.example.monewteam08.scheduler;
 
 import com.example.monewteam08.service.Interface.ArticleService;
-import lombok.RequiredArgsConstructor;
+import jakarta.annotation.PostConstruct;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class ArticleScheduler {
 
   private final ArticleService articleService;
+
+  public ArticleScheduler(ArticleService articleService) {
+    this.articleService = articleService;
+  }
+
+  @PostConstruct
+  public void init() {
+    articleService.save();
+  }
 
   @Scheduled(cron = "0 0 * * * *")
   public void fetchAndSaveArticles() {
