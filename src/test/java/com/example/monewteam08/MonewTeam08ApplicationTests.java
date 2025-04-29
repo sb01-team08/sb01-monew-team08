19d302ff2c5b48f46a7c7367f3fc8c838d564f49
silvarge <sep.non2@gmail.com>
2025-04-29 11:14:55 +0900

    :wrench: Chore: codecov test

--- src/test/java/com/example/monewteam08/MonewTeam08ApplicationTests.java
+++ src/test/java/com/example/monewteam08/MonewTeam08ApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.monewteam08;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MonewTeam08ApplicationTests {
 
-  @Test
-  void contextLoads() {
-  }
+//  @Test
+//  void contextLoads() {
+//  }
 
 }
